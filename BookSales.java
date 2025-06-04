1f99464fae118a5dd7f8e02cfc5008be696fc7c6
juktimath003 <juktimathshreya123@gmail.com>
2025-06-04 22:21:12 +0530

    save it

---
+++ BookSales.java
@@ -0,0 +1,20 @@
+package probState4;
+public class BookSales {
+    private String salerName;
+    private int numberOfSales;
+    private String salerId;
+
+    // Constructor
+    public BookSales(String salerName, int numberOfSales, String salerId) {
+        this.salerName = salerName;
+        this.numberOfSales = numberOfSales;
+        this.salerId = salerId;
+    }
+
+    // Display method
+    public void displaySalesDetails() {
+        System.out.println("Saler Name: " + salerName);
+        System.out.println("Number of Sales: " + numberOfSales);
+        System.out.println("Saler ID: " + salerId);
+    }
+}
